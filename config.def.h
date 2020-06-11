4ad83d98e311795e510972d0a4e05f5d5074678f
Rushil Kasetty <ggadget6@gmail.com>
2020-06-10 21:16:41 -0400

    Add volume controls
    Allows volume controls on keyboards to change volume

--- config.def.h
+++ config.def.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -72,6 +73,9 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
+const char *mutecmd[] = { "amixer", "-q", "set", "Master", "toggle", NULL };
+const char *upcmd[] = { "amixer", "-q", "set", "Master", "5%+", "unmute", NULL };
+const char *downcmd[] = { "amixer", "-q", "set", "Master", "5%-", "unmute", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -116,6 +120,9 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+    { 0,                            XF86XK_AudioMute, spawn,   {.v = mutecmd } },
+    { 0,                            XF86XK_AudioLowerVolume, spawn,   {.v = voldowncmd } },
+    { 0,                            XF86XK_AudioRaiseVolume, spawn,   {.v = volupcmd } },
 };
 
 /* button definitions */
